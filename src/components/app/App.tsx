7e34642ed308b9ee58d731d1fb2b34b62802520a
Ben Crook <ben@space48.com>
2019-06-01 01:42:30 +0100

    * Convert case in CSS rather than JS * Change some class names

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,13 @@
+import { InterfaceColumns, InterfaceTeamData, Team } from '../../types/data';
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import { CSSTransition } from 'react-transition-group';
 import getCurrentWeekNumber from '../../utilities/getWeekNumber';
-import appAnimations from './app-animations.module.css';
-import css from './app.module.css';
-import { InterfaceColumns, InterfaceTeamData, Team } from '../../types/data';
 import { baseUrl } from '../../utilities/urls';
 import { NewComment } from '../comment/NewComment';
 import { Comments } from '../comment/Comments';
+import css from './app.module.css';
+import appAnimations from './app-animations.module.css';
+import { CSSTransition } from 'react-transition-group';
 
 
 const commentTypes: string[] = ['good', 'bad', 'actions'];
@@ -19,10 +19,6 @@ const emptyTeamData: InterfaceColumns = {
 };
 
 const weekNumber = getCurrentWeekNumber(new Date());
-
-const convertFirstLetterToUppercase = (word : string) => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-};
 
 const App : React.FunctionComponent = () => {
     // todo: Include a landing page so the team can be chosen (if Black/Yellow will use this?)
@@ -53,24 +49,24 @@ const App : React.FunctionComponent = () => {
     const widthBasedStyling = {
         fontSize: fullWidthMode ? '20px' : '16px'
     };
-    const width = fullWidthMode ? css.fullWidthMode : '';
+    const width = fullWidthMode ? css.containerFullWidth : '';
 
     return (
         <div style={widthBasedStyling}>
             <div className={css.header}>
-                <h1>Space48's Retro Board</h1>
+                <h1>Space48 Retro Board - <span className={css.teamName}>{team}</span> team</h1>
                 <p>Click the button below to toggle full-width mode, this may be useful for displaying on TVs.</p>
                 <button onClick={() => setFullWidthMode(!fullWidthMode)}>Toggle Full Width Mode</button>
             </div>
 
             <CSSTransition in={shouldRender} timeout={200} classNames={{...appAnimations}}>
-                <div className={appAnimations.app}>
+                <div className={appAnimations.container}>
                     <div className={`${css.container} ${width}`}>
                         {commentTypes.map(commentType => (
                             <div className={`${css.column}`} key={commentType}>
                                 <div className={`${css.name}`}>
                                     <span className={`${css.colorPreview} background--${commentType}`} />
-                                    {convertFirstLetterToUppercase(commentType)}
+                                    {commentType}
                                 </div>
                                 <NewComment commentType={commentType} updateTeamData={updateTeamData}/>
                                 <Comments commentType={commentType} comments={teamData[commentType]}/>
