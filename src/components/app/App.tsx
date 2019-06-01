b7d42708b4b35b1a72d94bf8fa8c67725abf3e43
Ben Crook <ben@space48.com>
2019-06-01 01:11:29 +0100

    Remove columns module, it was unnecessarily adding complexity by adding 35 lines of code for the sake of saving 16

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { CSSTransition } from 'react-transition-group';
 import getCurrentWeekNumber from '../../utilities/getWeekNumber';
-import { Columns } from '../column/Columns';
 import appAnimations from './app-animations.module.css';
 import css from './app.module.css';
 import { InterfaceColumns, InterfaceTeamData, Team } from '../../types/data';
 import { baseUrl } from '../../utilities/urls';
+import { NewComment } from '../comment/NewComment';
+import { Comments } from '../comment/Comments';
 
 
 const commentTypes: string[] = ['good', 'bad', 'actions'];
@@ -18,6 +19,10 @@ const emptyTeamData: InterfaceColumns = {
 };
 
 const weekNumber = getCurrentWeekNumber(new Date());
+
+const convertFirstLetterToUppercase = (word : string) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+};
 
 const App : React.FunctionComponent = () => {
     // todo: Include a landing page so the team can be chosen (if Black/Yellow will use this?)
@@ -48,6 +53,7 @@ const App : React.FunctionComponent = () => {
     const widthBasedStyling = {
         fontSize: fullWidthMode ? '20px' : '16px'
     };
+    const width = fullWidthMode ? css.fullWidthMode : '';
 
     return (
         <div style={widthBasedStyling}>
@@ -59,10 +65,18 @@ const App : React.FunctionComponent = () => {
 
             <CSSTransition in={shouldRender} timeout={200} classNames={{...appAnimations}}>
                 <div className={appAnimations.app}>
-                    <Columns commentTypes={commentTypes}
-                             teamData={teamData}
-                             updateTeamData={updateTeamData}
-                             isFullWidthMode={fullWidthMode} />
+                    <div className={`${css.container} ${width}`}>
+                        {commentTypes.map(commentType => (
+                            <div className={`${css.column}`} key={commentType}>
+                                <div className={`${css.name}`}>
+                                    <span className={`${css.colorPreview} background--${commentType}`} />
+                                    {convertFirstLetterToUppercase(commentType)}
+                                </div>
+                                <NewComment commentType={commentType} updateTeamData={updateTeamData}/>
+                                <Comments commentType={commentType} comments={teamData[commentType]}/>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </CSSTransition>
         </div>
