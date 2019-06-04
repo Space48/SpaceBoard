970c65da6909a48fc3e77bf1ce5ed5699c88b216
Ben Crook <ben@space48.com>
2019-06-04 14:55:44 +0100

    Update production URL

---
+++ build/precache-manifest.62d0949d13aa65c6603a70859cbebc7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6fcd8be08568c48dc6d7eda8d554c9",
+    "url": "/newretro/index.html"
+  },
+  {
+    "revision": "45e34fb5bfd6da631e74",
+    "url": "/newretro/static/css/main.0a06e5f2.chunk.css"
+  },
+  {
+    "revision": "15e74a44b857c9980fb5",
+    "url": "/newretro/static/js/2.b17a35cf.chunk.js"
+  },
+  {
+    "revision": "45e34fb5bfd6da631e74",
+    "url": "/newretro/static/js/main.0e922536.chunk.js"
+  },
+  {
+    "revision": "99e76fbe301b6daeceb7",
+    "url": "/newretro/static/js/runtime~main.cc432a58.js"
+  }
+]);
