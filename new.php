4188cfccb7b9e96fd88f2b4db8fe998d445a8945
Dan Carlyon <dan.carlyon@space48.com>
2019-06-18 20:39:11 +0100

    Modify saved data

--- new.php
+++ new.php
@@ -2,11 +2,16 @@
 
 $date = new DateTime();
 $week = $date->format("W");
-$boardData = json_decode(file_get_contents('teams/red/week' . $week . '.json'));
+$file = dirname(__FILE__) . '/teams/red/week' . $week . '.json';
+if (!file_exists($file)) {
+    $dataStructure = ['good' => [], 'bad' => [], 'actions' => []];
+    file_put_contents($file, json_encode($dataStructure));
+}
+$boardData = json_decode(file_get_contents($file));
 $typeToAdd = $_REQUEST['commentType'];
 $textToAdd = $_REQUEST['text'];
 
-$boardData->{$typeToAdd}[] = $textToAdd;
+$boardData->{$typeToAdd}[time()] = ['message' => $textToAdd, 'upvotes' => 0, 'author' => ''];
 
 $data = json_encode($boardData);
 file_put_contents('teams/red/week' . $week . '.json', $data);
