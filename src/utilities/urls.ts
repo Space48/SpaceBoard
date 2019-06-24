d97bd38963c5628fcfbb08c84e935f99f8b89280
Dan Carlyon <dan.carlyon@space48.com>
2019-06-24 15:10:43 +0100

    Change url for production

--- src/utilities/urls.ts
+++ src/utilities/urls.ts
@@ -5,7 +5,7 @@ export const baseUrl = () : string => {
     const mode = process.env.NODE_ENV;
     switch (mode) {
         case 'production': {
-            return '/retro/';
+            return './';
         }
         case 'development': {
             return './'
